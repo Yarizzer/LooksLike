3370b2933027b08092aa00677b0a5832c8291464
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-27 09:21:46 +0200

    AppUILayerStyleService refactoreed

--- LooksLike/Application/AppCore/Layers/UILayer/Services/Style/AppUILayerStyleServiceType.swift
+++ LooksLike/Application/AppCore/Layers/UILayer/Services/Style/AppUILayerStyleServiceType.swift
@@ -7,26 +7,26 @@
 
 import UIKit
 
-protocol AppUILayerStyleServiceType {
-    //Colors
-    var colorClear: UIColor { get }
-    var colorWhite: UIColor { get }
-    var colorBlack: UIColor { get }
-    var colorLightGray: UIColor { get }
-    var colorDarkGray: UIColor { get }
-    var colorRed: UIColor { get }
-    var colorSoftGreen: UIColor { get }
-    var colorGreen: UIColor { get }
-    var colorSoftYellow: UIColor { get }
-    var colorSoftBlue: UIColor { get }
-    var colorBlue: UIColor { get }
-    var colorSoftPurple: UIColor { get }
-    var colorPurple: UIColor { get }
-    //Fonts
-    var labelTitleFontLarge: UIFont { get }
-    var labelTitleFontMedium: UIFont { get }
-    var labelTitleFontSmall: UIFont { get }
-    //Buttons
-    var layerBorderWidth: CGFloat { get }
-    var layerLineWidth: CGFloat { get }
+protocol AppUILayerStyleServiceType: AnyObject {
+  //Colors
+  var colorClear: UIColor { get }
+  var colorWhite: UIColor { get }
+  var colorBlack: UIColor { get }
+  var colorLightGray: UIColor { get }
+  var colorDarkGray: UIColor { get }
+  var colorRed: UIColor { get }
+  var colorSoftGreen: UIColor { get }
+  var colorGreen: UIColor { get }
+  var colorSoftYellow: UIColor { get }
+  var colorSoftBlue: UIColor { get }
+  var colorBlue: UIColor { get }
+  var colorSoftPurple: UIColor { get }
+  var colorPurple: UIColor { get }
+  //Fonts
+  var labelTitleFontLarge: UIFont { get }
+  var labelTitleFontMedium: UIFont { get }
+  var labelTitleFontSmall: UIFont { get }
+  //Buttons
+  var layerBorderWidth: CGFloat { get }
+  var layerLineWidth: CGFloat { get }
 }
