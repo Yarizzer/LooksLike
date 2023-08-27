4243ff6c64b7e8e7ddf490537837838b5a09126d
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-27 09:50:38 +0200

    MainSceneViewController refactored

--- LooksLike/Modules/MainScene/View/MainSceneViewController.swift
+++ LooksLike/Modules/MainScene/View/MainSceneViewController.swift
@@ -9,52 +9,52 @@
 import UIKit
 
 class MainSceneViewController: BaseViewController<MainSceneInteractable> {
-    // MARK: View lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setup()
-    }
+  // MARK: View lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        interactor?.makeRequest(requestType: .viewIsReady)
-    }
+    setup()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        interactor?.makeRequest(requestType: .viewWillDisappear)
-    }
+    interactor?.makeRequest(requestType: .viewIsReady)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
     
-    private func setup() {
-        view.backgroundColor = AppCore.shared.uiLayer.style.colorDarkGray
-        
-        label.font = AppCore.shared.uiLayer.style.labelTitleFontMedium
-        label.minimumScaleFactor = Constants.minScaleFactor
-        label.textColor = AppCore.shared.uiLayer.style.colorLightGray
-        
-        interactor?.makeRequest(requestType: .initialSetup)
-    }
+    interactor?.makeRequest(requestType: .viewWillDisappear)
+  }
+  
+  private func setup() {
+    view.backgroundColor = AppCore.shared.uiLayer.style.colorDarkGray
     
-    @IBOutlet private weak var captureView: CaptureView!
-    @IBOutlet private weak var label: UILabel!
+    label.font = AppCore.shared.uiLayer.style.labelTitleFontMedium
+    label.minimumScaleFactor = Constants.minScaleFactor
+    label.textColor = AppCore.shared.uiLayer.style.colorLightGray
+    
+    interactor?.makeRequest(requestType: .initialSetup)
+  }
+  
+  @IBOutlet private weak var captureView: CaptureView!
+  @IBOutlet private weak var label: UILabel!
 }
 
 extension MainSceneViewController: MainSceneViewControllerType {
-    func update(viewModelDataType: MainSceneViewControllerViewModel.ViewModelDataType) {
-        switch viewModelDataType {
-        case .initialSetup(let model):  captureView.inject(model: model.getCaptureViewModel())
-        case .viewIsReady:              captureView.awakeSession()
-        case .viewWillDisappear:        captureView.sleepSession()
-        case .updateLabel(let data):    label.text = data
-        }
+  func update(viewModelDataType: MainSceneViewControllerViewModel.ViewModelDataType) {
+    switch viewModelDataType {
+    case .initialSetup(let model):  captureView.inject(model: model.getCaptureViewModel())
+    case .viewIsReady:              captureView.awakeSession()
+    case .viewWillDisappear:        captureView.sleepSession()
+    case .updateLabel(let data):    label.text = data
     }
+  }
 }
 
 extension MainSceneViewController {
 	private struct Constants {
-        static let minScaleFactor: CGFloat = 0.5
-    }
+    static let minScaleFactor: CGFloat = 0.5
+  }
 }
